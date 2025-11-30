f73a6ad2ab0132697626f9f977a9d53fd9ab1418
Amit Dey <lynkamit@gmail.com>
2025-11-30 09:48:55 -0700

    Update project files

--- src/components/InvoiceForm.js
+++ src/components/InvoiceForm.js
@@ -13,7 +13,7 @@ const InvoiceForm = ({ setInvoices, invoices = [] }) => {
     clientCarrier: "",
     clientCarrierInvoiceNumber: "",
     invoiceDate: new Date().toISOString().split('T')[0],
-    driverName: "Lucky",
+    driverName: "Lucky1",
     pickupDate: "",
     pickupAddress: "",
     deliveryDate: "",
